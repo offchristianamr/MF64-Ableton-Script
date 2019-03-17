fe9a66e4301810d19c1e58f6e92c0480efe41803
offchristianamr <48657157+offchristianamr@users.noreply.github.com>
2019-03-17 13:29:47 -0700

    Update __init__.py
    Changed line 5 because it was returning an error. New code taken from Push/__init__ (same code is in many other remote scripts)

--- Midi_Fighter_64/__init__.py
+++ Midi_Fighter_64/__init__.py
@@ -2,7 +2,7 @@ from _Framework.Capabilities import CONTROLLER_ID_KEY, PORTS_KEY, NOTES_CC, SCRI
 from Midi_Fighter_64 import Midi_Fighter_64
 
 def create_instance(c_instance):
-	return Midi_Fighter_64(c_instance)
+    return Midi_Fighter_64(c_instance=c_instance)
 
 def get_capabilities():
 	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2580, product_ids=[8], model_name='Midi Fighter 64'),
