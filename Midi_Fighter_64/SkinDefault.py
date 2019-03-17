041be4ee86483c26d24a4d211a3220bc829ffd22
offchristianamr <48657157+offchristianamr@users.noreply.github.com>
2019-03-17 13:26:35 -0700

    Update SkinDefault.py
    Began updating the colors to better reflect Live 10's pushbase

--- Midi_Fighter_64/SkinDefault.py
+++ Midi_Fighter_64/SkinDefault.py
@@ -2,11 +2,11 @@
 from _Framework.Skin import Skin
 from _Framework.ButtonElement import Color
 from Colors import Rgb, Pulse, Blink
-GREEN = Color(1)
-GREEN_BLINK = Color(2)
-RED = Color(3)
-RED_BLINK = Color(4)
-AMBER = Color(5)
+GREEN = Color(21)
+RED = Color(5)
+GREEN_BLINK_SLOW = Color(2)
+RED_BLINK_SLOW = Color(4)
+AMBER = Color(9)
 
 class Defaults:
 
@@ -22,14 +22,14 @@ class BiLedColors:
         ClipStopped = AMBER
         ClipStarted = GREEN
         ClipRecording = RED
-        ClipTriggeredPlay = GREEN_BLINK
-        ClipTriggeredRecord = RED_BLINK
+        ClipTriggeredPlay = GREEN_BLINK_SLOW
+        ClipTriggeredRecord = RED_BLINK_SLOW
         ClipEmpty = Color(0)
         Scene = Color(0)
-        SceneTriggered = GREEN_BLINK
+        SceneTriggered = GREEN_BLINK_SLOW
         NoScene = Color(0)
         StopClip = Color(0)
-        StopClipTriggered = GREEN_BLINK
+        StopClipTriggered = GREEN_BLINK_SLOW
         RecordButton = Color(0)
 
     class Zooming:
